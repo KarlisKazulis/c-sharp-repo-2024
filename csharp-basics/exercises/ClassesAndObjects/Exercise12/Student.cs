793d138774ef480de1c61cda2f5e46e9c195f96c
Karlis Kazulis <karlis.kazulis@gmail.com>
2024-09-06 16:40:37 +0300

    Classes and objects tasks done finally!

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise12/Student.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise12
+{
+    public interface IStudent
+    {
+        string[] TestsTaken { get; }
+        void TakeTest(ITestpaper paper, string[] answers);
+    }
+
+    class Student : IStudent
+    {
+        private List<string> _testsTaken;
+
+        public Student()
+        {
+            _testsTaken = new List<string> { "No tests taken" };
+        }
+
+        public string[] TestsTaken
+        {
+            get { return _testsTaken.ToArray(); }
+        }
+
+        public void TakeTest(ITestpaper paper, string[] answers)
+        {
+            int _correctAnswers = 0;
+            int _totalQuestions = paper.GetMarkScheme().Length;
+
+            for (int i = 0; i < _totalQuestions; i++)
+            {
+                if (answers[i] == paper.GetMarkScheme()[i])
+                {
+                    _correctAnswers++;
+                }
+            }
+
+            double _percentage = ((double)_correctAnswers / _totalQuestions) * 100;
+            
+            int _passMark = int.Parse(paper.GetPassMark().TrimEnd('%'));
+
+            string _result;
+            if (_percentage >= _passMark)
+            {
+                _result = "Passed!";
+            }
+            else
+            {
+                _result = "Failed!";
+            }
+
+            string _testResult = $"{paper.GetSubject()}: {_result} ({_percentage:0.##}%)";
+
+            if (_testsTaken.Count == 1 && _testsTaken[0] == "No tests taken")
+            {
+                _testsTaken.Clear();
+            }
+
+            _testsTaken.Add(_testResult);
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise4/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie1 = new Movie("Casino Royale", "Eon Productions", "PG-13");
+            Movie movie2 = new Movie("Glass", "Buena Vista International", "PG-13");
+            Movie movie3 = new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures", "PG");
+
+            Movie[] movies = { movie1, movie2, movie3 };
+
+            Movie[] pgMovies = GetPG(movies);
+
+            Console.WriteLine("PG movies are: ");
+            foreach (Movie movie in pgMovies)
+            {
+                Console.WriteLine(movie.PrintMovie());
+            }
+        }
+
+        public static Movie[] GetPG(Movie[] movies) {
+            
+            List<Movie> pgMovieList = new List<Movie>();
+
+            foreach (Movie movie in movies)
+            {
+                if (movie.GetMovieRating() == "PG")
+                {
+                    pgMovieList.Add(movie);
+                }
+            }
+
+            return pgMovieList.ToArray();
+        }
+    }
+}
